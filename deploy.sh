a2d2791b7f94e8c7ed36bb855de4892964814b5e
Memória de Cálculo <memoriadcalculo@gmail.com>
2025-07-12 15:52:31 -0300

    Fix Deploy

--- deploy.sh
+++ deploy.sh
@@ -3,19 +3,19 @@ echo "Posicionando na Home"
 cd $HOME
 
 echo "Ativando ambiente virtual"
-source .virtualenvs/avalim/bin/activate
+source venv/bin/activate
 
-echo "Posicionando no projeto MC"
-cd ./MC
+echo "Posicionando no projeto AvalIm"
+cd ./avalim
 
 echo "Alterando DEBUG para falso"
-sed -i 's/DEBUG = True/DEBUG = False/g' ./MC/settings.py
+sed -i 's/DEBUG = True/DEBUG = False/g' ./avalim/settings.py
 
 echo "Instalando pacotes"
 pip3 install -r ./requirements.txt
 
 echo "Executando migrate"
-# python3 ./manage.py migrate
+python3 ./manage.py migrate
 
 echo "Executando collectstatic"
 python ./manage.py collectstatic --noinput
